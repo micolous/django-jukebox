ccd27576645e8c572851f10ee17825a7d9ddd126
l11gctaylor <l11gctaylor@575cad68-ddf0-11de-9bce-518a61f59f6d>
2009-11-30 21:26:38 +0000

    Song model and some template directories.

--- apps/music_db/models.py
+++ apps/music_db/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Music):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255, blank=True)
+    # Song length (seconds)
+    length = models.IntegerField(blank=True, null=True)
+    track_number = models.IntegerField(blank=True, null=True)
+    disc_number = models.IntegerField(blank=True, null=True)
+    # Number of times the song has been requested.
+    request_count = models.IntegerField(default=0)
+    # Average of all ratings for this song.
+    rating = models.FloatField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['artist', 'disc_number', 'track_number', 'title']
+    
+    def __unicode__(self):
+        return "%s - %s" % (self.artist, self.title)
