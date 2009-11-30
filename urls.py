bef5246e5aa0b1c5178eec30f8af8fc2589d2ae3
l11gctaylor <l11gctaylor@575cad68-ddf0-11de-9bce-518a61f59f6d>
2009-11-30 20:47:22 +0000

    Bringing in project skeleton.

---
+++ urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^djangojuke/', include('djangojuke.foo.urls')),
+
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    (r'^admin/', include(admin.site.urls)),
+)
+
+# Serve Media via Django if we're running a development site. This is primarily
+# used for local development and should never be done in a production
+# environment.
+if settings.DJANGO_SERVE_MEDIA:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
