3e386096e42e04de6c9cc3a5d8970787005689d3
l11gctaylor <l11gctaylor@575cad68-ddf0-11de-9bce-518a61f59f6d>
2009-12-08 21:48:47 +0000

    Make a lot of fields inline editable for now. Ugly but functional.

--- apps/music_db/admin.py
+++ apps/music_db/admin.py
@@ -5,9 +5,11 @@ from django.contrib import admin
 from apps.music_db.models import Song
 
 class SongAdmin(admin.ModelAdmin):
-    list_display = ('artist', 'album', 'title', 'disc_number', 'track_number',
-                    'genre', 'request_count', 'rating', 'file',
+    list_display = ('id', 'artist', 'album', 'title', 'disc_number', 
+                    'track_number', 'genre', 'request_count', 'rating', 'file',
                     'allow_random_play')
+    list_editable = ('artist', 'album', 'title', 'disc_number', 
+                    'track_number', 'genre', 'allow_random_play')
     date_hierarchy = 'time_added'
     search_fields = ['title', 'artist', 'genre']
 admin.site.register(Song, SongAdmin)
