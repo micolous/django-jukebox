832b84af23d3fa92544cf5626f93b10893a71238
l11gctaylor <l11gctaylor@575cad68-ddf0-11de-9bce-518a61f59f6d>
2009-12-10 15:26:52 +0000

    Make experimental jtui use standard.html, yank the star rating thing because it is poop.

--- apps/juketunes_ui/views/main.py
+++ apps/juketunes_ui/views/main.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.conf import settings
 
 def view_main(request):
     """
     Main view for the music player.
     """
     pagevars = {
-        "page_title": "Django JukeBox Beta",
+        "page_title": settings.PROGRAM_NAME,
     }
 
     context_instance = RequestContext(request)
