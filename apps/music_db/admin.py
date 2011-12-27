d88318a037aa2a57b1b21d81bbabd66da08d3970
Michael Farrell <micolous+git@gmail.com>
2011-12-28 01:15:54 +1030

    Remove track and disk number from django admin.

--- apps/music_db/admin.py
+++ apps/music_db/admin.py
@@ -5,11 +5,9 @@ from django.contrib import admin
 from apps.music_db.models import Song
 
 class SongAdmin(admin.ModelAdmin):
-    list_display = ('id', 'artist', 'album', 'title', 'disc_number', 
-                    'track_number', 'genre', 'request_count', 'rating', 'get_local_path',
+    list_display = ('id', 'artist', 'album', 'title', 'genre', 'request_count', 'rating', 'get_local_path',
                     'allow_random_play')
-    list_editable = ('artist', 'album', 'title', 'disc_number', 
-                    'track_number', 'genre', 'allow_random_play')
+    list_editable = ('artist', 'album', 'title', 'genre', 'allow_random_play')
     date_hierarchy = 'time_added'
     search_fields = ['title', 'artist', 'album', 'genre']
 admin.site.register(Song, SongAdmin)
