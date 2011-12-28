8a2972f6188e41e719376a9889aa248d7745ec5f
Michael Farrell <micolous+git@gmail.com>
2011-12-28 15:26:09 +1030

    Implemented password change system.  Added link back to main screen.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,6 +2,36 @@ from django.conf.urls.defaults import *
 
 # Generic views that don't require actual internal view code of their own.
 urlpatterns = patterns('django.contrib.auth.views',
-    url(r'^logout/$', 'logout_then_login', name='logout'),
-    url(r'^login/$', 'login', {'template_name': 'login.html'}, name='login'),
+	url(
+		r'^logout/$',
+		'logout_then_login',
+		name='logout'
+	),
+	
+	url(
+		r'^login/$',
+		'login',
+		dict(
+			template_name='login.html'
+		),
+		name='login'
+	),
+	
+	url(
+		r'^change_password/$',
+		'password_change',
+		dict(
+			template_name='password_change_form.html'
+		),
+		name='change_password'
+	),
+	
+	url(
+		r'^change_password/done/$',
+		'password_change_done',
+		dict(
+			template_name='password_change_done.html'
+		),
+		name='password_change_done'
+	),
 )
